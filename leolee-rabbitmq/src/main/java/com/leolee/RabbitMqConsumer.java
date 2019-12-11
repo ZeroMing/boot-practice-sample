cd1e7e06c438d47847d00fd1d5f6d45007854b15
ming_lee <1336942608@qq.com>
2019-12-11 23:31:57 +0800

    异步线程处理任务案例

---
+++ leolee-rabbitmq/src/main/java/com/leolee/RabbitMqConsumer.java
@@ -0,0 +1,18 @@
+package com.leolee;
+
+/**
+ * @author: zeroming@163.com
+ * @version:
+ * @date: 2019年11月29 22时49分
+ */
+public class RabbitMqConsumer {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+}
